91b856ed3adcb2c819df5364c0cceffb10415474
eiya <x>
2023-01-10 18:36:10 +0900

    firebaseconfig削除

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'auth/firebase_user_provider.dart';
 import 'auth/auth_util.dart';
 
-import 'backend/firebase/firebase_config.dart';
+// import 'backend/firebase/firebase_config.dart';
 import 'flutter_flow/flutter_flow_theme.dart';
 import 'flutter_flow/flutter_flow_util.dart';
 import 'flutter_flow/internationalization.dart';
@@ -16,7 +16,7 @@ import 'routes/routes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await initFirebase();
+  // await initFirebase();
 
   await FlutterFlowTheme.initialize();
 
